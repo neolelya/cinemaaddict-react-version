ba6c2fe490c36ebc1c45e80252f95f7b833d9951
Volha Trehubava <neolelya@gmail.com>
2020-05-06 19:34:07 +0200

    gh-pages updated

---
+++ precache-manifest.0a8718b85614c65b27f5b7aeaee05201.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6241025eee4b184ff3b581e1f9901f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c11081c2d0c8835a63",
+    "url": "/static/css/main.fc39a8d0.chunk.css"
+  },
+  {
+    "revision": "f6dec079493cbdb9b137",
+    "url": "/static/js/2.8e38089c.chunk.js"
+  },
+  {
+    "revision": "867f57e759ba35b263312ace744f166f",
+    "url": "/static/js/2.8e38089c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13c11081c2d0c8835a63",
+    "url": "/static/js/main.84417230.chunk.js"
+  },
+  {
+    "revision": "c570ccdc7a331f415f68",
+    "url": "/static/js/runtime-main.c00621f1.js"
+  },
+  {
+    "revision": "ccb112edb63776487554e0d11e2bf8ec",
+    "url": "/static/media/angry.ccb112ed.png"
+  },
+  {
+    "revision": "b8947d20ef3086ab0844aa274d3671e9",
+    "url": "/static/media/background.b8947d20.png"
+  },
+  {
+    "revision": "30b43b0fd1634b13c415e8ad34a98773",
+    "url": "/static/media/bitmap@2x.30b43b0f.png"
+  },
+  {
+    "revision": "1316be1ad0fb1295a5cf5f9665944dfa",
+    "url": "/static/media/icon-favorite-active.1316be1a.svg"
+  },
+  {
+    "revision": "43ca5d5d479d1bdd0394045dd3b6ca03",
+    "url": "/static/media/icon-favorite.43ca5d5d.svg"
+  },
+  {
+    "revision": "7a2ab196e69ffc745f9563183b94abe6",
+    "url": "/static/media/icon-watched-active.7a2ab196.svg"
+  },
+  {
+    "revision": "4e1cd4d02a2e0e279a2aac28336e6c5a",
+    "url": "/static/media/icon-watched.4e1cd4d0.svg"
+  },
+  {
+    "revision": "30bb292d2e0e8bece499959ea972c227",
+    "url": "/static/media/icon-watchlist-active.30bb292d.svg"
+  },
+  {
+    "revision": "89898333aff7f3e6ca4330c07d35c3cf",
+    "url": "/static/media/icon-watchlist.89898333.svg"
+  },
+  {
+    "revision": "9e5158db60b13b71cacbf22dcc10875a",
+    "url": "/static/media/made-for-each-other.9e5158db.png"
+  },
+  {
+    "revision": "e6fcd3eb8c1f83bb5294c6c30fadec20",
+    "url": "/static/media/popeye-meets-sinbad.e6fcd3eb.png"
+  },
+  {
+    "revision": "ad7efb780b981b92e965b72c6b0dfc45",
+    "url": "/static/media/puke.ad7efb78.png"
+  },
+  {
+    "revision": "c5bc5dc3862440d7dcd11fb353c1cad1",
+    "url": "/static/media/sagebrush-trail.c5bc5dc3.jpg"
+  },
+  {
+    "revision": "191a6dec1bb61d0086336170b8d974f1",
+    "url": "/static/media/santa-claus-conquers-the-martians.191a6dec.jpg"
+  },
+  {
+    "revision": "fe8e98288cfcd135ea658b5b8ce16153",
+    "url": "/static/media/sleeping.fe8e9828.png"
+  },
+  {
+    "revision": "f0cd583f0a68b270d504ab94aeb9fd02",
+    "url": "/static/media/smile.f0cd583f.png"
+  },
+  {
+    "revision": "f3de8723301b5ec4ef53bf6f8513e70c",
+    "url": "/static/media/the-dance-of-life.f3de8723.jpg"
+  },
+  {
+    "revision": "bbe534a39e5f569781ce75ea53b0fcc4",
+    "url": "/static/media/the-great-flamarion.bbe534a3.jpg"
+  },
+  {
+    "revision": "1e64d78a88bc45d50e815d10638a2c89",
+    "url": "/static/media/the-man-with-the-golden-arm.1e64d78a.jpg"
+  },
+  {
+    "revision": "102b6e35c5925636415ee95ae3ba12f9",
+    "url": "/static/media/trophy.102b6e35.png"
+  }
+]);
